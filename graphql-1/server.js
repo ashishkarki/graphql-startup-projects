9b10c61061dcee3870766d72adff8050045bca02
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-05-06 23:00:07 +0545

    adding graphql schema

--- graphql-1/server.js
+++ graphql-1/server.js
@@ -6,6 +6,30 @@ import { DEFAULTS } from './constants.js'
 
 const app = express()
 
+// graphql schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+    helloWithoutNullReturn: String!
+    welcome(name: String, dayOfWeek: String!): String
+  }
+`)
+
+const root = {
+  hello: () => {
+    // return 'Hello World'
+    return null
+  },
+  helloWithoutNullReturn: () => {
+    // return null
+    return 'Hello that cannot return null'
+  },
+  welcome: args => {
+    console.log(args)
+    return `Hey person named: ${args.name}. Today is ${args.dayOfWeek}`
+  },
+}
+
 // setup routes
 app.use(
   '/graphql',
